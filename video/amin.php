ff3cc3c9c4d613389bbefc50e1f4783ec75016d8
ClowClow <c.servieres@simplon-charleville.fr>
2019-05-29 10:50:03 +0200

    ajout de la page d'amin (cf fichier video)

---
+++ video/amin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="amin.css">
+    <title>amin</title>
+</head>
+<body>
+    <main role="main">
+        <div class="image">
+                <img src="logoinde.png" alt="logoinde" style="width: 100%; height:60px">
+        </div>
+    <div class="user"><br>
+        <form action="resultat.php" method="POST">
+        <label for="username">Utilisateur:</label><br>
+        <input type="text" id="username" name="username"><br>
+        <label for="pass">Mot de passe:</label><br>
+        <input type="password" id="pass" name="pass"  required><br>
+        <input type="submit" value="Connexion">
+        </form>
+    </main>
+
+    </div>
+
+
+</body>
+</html>
